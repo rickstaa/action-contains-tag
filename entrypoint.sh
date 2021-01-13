b7719881b71b18c74b00c9bcf998e5a36069d227
Rick Staa <rick.staa@outlook.com>
2021-01-13 12:04:21 +0100

    :loud_sound: Adds extra debug statement

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,8 @@ set -eux
 cd "${GITHUB_WORKSPACE}" || exit
 
 echo "Commit that triggered: ${GITHUB_SHA}"
+echo "The branches that are presetn on the remote:"
+git branch -a
 echo "${INPUT_TAG}"
 tag="${INPUT_TAG#'refs/tags/'}" # Remove possible refs/tags prefix
 
